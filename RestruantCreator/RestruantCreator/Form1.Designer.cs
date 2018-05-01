b7df1b94a54cf3d241da9b0587d7223d060d018f
ecejmsr <ecejmsr>
2018-05-01 01:03:49 -0400

    Added my Resturant Creator. It is a Forms App

---
+++ RestruantCreator/RestruantCreator/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace RestruantCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.AddressText = new System.Windows.Forms.TextBox();
+            this.ZipCodeText = new System.Windows.Forms.TextBox();
+            this.PhoneNumberText = new System.Windows.Forms.TextBox();
+            this.RatingText = new System.Windows.Forms.TextBox();
+            this.ReviewText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NameText
+            // 
+            this.NameText.Location = new System.Drawing.Point(231, 54);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(168, 20);
+            this.NameText.TabIndex = 0;
+            this.NameText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // AddressText
+            // 
+            this.AddressText.Location = new System.Drawing.Point(231, 104);
+            this.AddressText.Name = "AddressText";
+            this.AddressText.Size = new System.Drawing.Size(168, 20);
+            this.AddressText.TabIndex = 1;
+            // 
+            // ZipCodeText
+            // 
+            this.ZipCodeText.Location = new System.Drawing.Point(231, 153);
+            this.ZipCodeText.Name = "ZipCodeText";
+            this.ZipCodeText.Size = new System.Drawing.Size(168, 20);
+            this.ZipCodeText.TabIndex = 2;
+            this.ZipCodeText.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // PhoneNumberText
+            // 
+            this.PhoneNumberText.Location = new System.Drawing.Point(231, 213);
+            this.PhoneNumberText.Name = "PhoneNumberText";
+            this.PhoneNumberText.Size = new System.Drawing.Size(168, 20);
+            this.PhoneNumberText.TabIndex = 3;
+            // 
+            // RatingText
+            // 
+            this.RatingText.Location = new System.Drawing.Point(231, 262);
+            this.RatingText.Name = "RatingText";
+            this.RatingText.Size = new System.Drawing.Size(168, 20);
+            this.RatingText.TabIndex = 4;
+            // 
+            // ReviewText
+            // 
+            this.ReviewText.Location = new System.Drawing.Point(231, 316);
+            this.ReviewText.Name = "ReviewText";
+            this.ReviewText.Size = new System.Drawing.Size(168, 20);
+            this.ReviewText.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Resturant Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Phone Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Zip Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(186, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Rating";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(173, 323);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Review";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(231, 360);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 13;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(324, 360);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 14;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ReviewText);
+            this.Controls.Add(this.RatingText);
+            this.Controls.Add(this.PhoneNumberText);
+            this.Controls.Add(this.ZipCodeText);
+            this.Controls.Add(this.AddressText);
+            this.Controls.Add(this.NameText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameText;
+        private System.Windows.Forms.TextBox AddressText;
+        private System.Windows.Forms.TextBox ZipCodeText;
+        private System.Windows.Forms.TextBox PhoneNumberText;
+        private System.Windows.Forms.TextBox RatingText;
+        private System.Windows.Forms.TextBox ReviewText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ RestruantCreator/RestruantCreator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace RestruantCreator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string dir = AppDomain.CurrentDomain.BaseDirectory;
+        string fileName = "ResturantList";
+        List<Resturant> listOfRestruants = new List<Resturant>();
+
+        public class Resturant
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string PhoneNumber { get; set; }
+            public string ZipCode { get; set; }
+            public double AveRate { get; set; }
+            public string Rating { get; set; }
+            public string Review { get; set; }
+            public List<double> RatingList = new List<double>();
+            public List<string> ReviewList = new List<string>();
+
+            public Resturant(string name, string address, string phonenumber, string zipcode, string rating, string review)
+            {
+                Name = name;
+                Address = address;
+                PhoneNumber = phonenumber;
+                ZipCode = zipcode;
+                RatingList.Add(Convert.ToDouble(rating));
+                ReviewList.Add(review);
+            }
+
+            public bool Equals(Resturant other)
+            {
+                if (other == null) return false;
+                if (this.Name != other.Name) return false;
+                if (this.Address != other.Address) return false;
+                if (this.ZipCode != other.ZipCode) return false;
+                return true;
+            }
+
+            public double AddToRatings(double rate)
+            {
+                RatingList.Add(rate);
+                return rate;
+            }
+
+            public void AddToReviews(string review)
+            {
+                ReviewList.Add(review);
+            }
+        }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Resturant rest = new Resturant(NameText.Text, AddressText.Text, PhoneNumberText.Text,ZipCodeText.Text,RatingText.Text, ReviewText.Text);
+                if (!listOfRestruants.Contains(rest))
+                {
+                listOfRestruants.Add(rest);
+                }
+                else
+                {
+                listOfRestruants.Add(rest);
+                }
+            NameText.Clear();
+            AddressText.Clear();
+            ZipCodeText.Clear();
+            PhoneNumberText.Clear();
+            RatingText.Clear();
+            ReviewText.Clear();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(dir + fileName, JsonConvert.SerializeObject(listOfRestruants));
+            Application.Exit();
+        }
+    }
+}
